a68ab05e96b19ab3e08dc56cee4cb3bfe3b9e10f
mkhachab <159106255+mkhachab@users.noreply.github.com>
2024-05-27 22:57:50 +0300

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,102 @@
+<section id="footer">
+        <!-- Footer -->
+<footer class="text-center text-lg-start bg-dark text-white">
+    <!-- Section: Social media -->
+    <section
+      class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+    >
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+       
+        <a href="https://www.instagram.com/town32.tyre/" target="_blank" class="ml-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="mailto:info@town32.com" target=:"_blank" class="ml-4 text-reset">
+  <i class="far fa-envelope"></i>
+</a>
+
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="sub-footer">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+             Town32 
+            </h6>
+            <p>
+                Whether you book a hotel online, on the phone, or through a travel agent, it should be a simple process.
+                 It should be easy to contact a knowledgeable, helpful person
+                 
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Quick links
+            </h6>
+         
+
+            <p>
+              <a href="../gallery.php" class="text-reset">Gallery</a>
+            </p>
+            <p>
+              <a href="../contact.php" class="text-reset">Contact us</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Contact
+            </h6>
+            <p><i class="fas fa-home me-3"></i>Tyre, South Lebanon</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+             Town32@gmail.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> +961 81 682 782</p>
+            <p><i class="fas fa-phone me-3"></i> +961 07 741 031</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+   
+   
+  </footer>
+  <!-- Footer -->
+  </section>
+     
+
+<script  src="../assets/js/index.js"></script>
+
+<!-- Jquery Time picker  -->
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+
+     </body>
+</html>
+
